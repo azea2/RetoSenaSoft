03125c2418dd7b04e407f9285c7fb955497a174e
Sperbec <sperbec@gmail.com>
2021-10-21 15:43:35 -0500

    Django backend

---
+++ historiasc/polls/views.py
@@ -0,0 +1,15 @@
+from django import http
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Pacientes
+
+def index(request):
+    return HttpResponse("Index")
+
+def vistaPacientes(request):
+    
+    x = Pacientes.objects.all()
+
+    return HttpResponse(x)
